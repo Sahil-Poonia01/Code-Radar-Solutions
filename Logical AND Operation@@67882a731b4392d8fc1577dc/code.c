7b48e29c7a9cc7b42f7f0fa66b15cae11942383e
Sahil-Poonia01 <sp6394159@gmail.com>
2025-02-09 19:24:41 +0530

    Solution to Logical AND Operation

--- Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
+++ Logical AND Operation@@67882a731b4392d8fc1577dc/code.c
@@ -4,7 +4,7 @@ int main() {
     int num1, num2;  // Variables to store the two numbers
 
     // Taking input for the two numbers
-    
+
     scanf("%d", &num1);
 
     
@@ -12,9 +12,9 @@ int main() {
 
     // Checking if both numbers are greater than zero using the && operator
     if (num1 > 0 && num2 > 0) {
-        printf("Both numbers are greater than zero.\n");
+        printf("True\n");
     } else {
-        printf("One or both numbers are not greater than zero.\n");
+        printf("False\n");
     }
 
     return 0;
