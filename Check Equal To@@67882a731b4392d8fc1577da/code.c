30ff6dfa04d3038fd8ff6590c7a6551389b751c5
Sahil-Poonia01 <sp6394159@gmail.com>
2025-02-09 19:19:26 +0530

    Solution to Check Equal To

---
+++ Check Equal To@@67882a731b4392d8fc1577da/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;  // Variables to store the two numbers
+
+    
+    scanf("%d", &num1);
+
+
+    scanf("%d", &num2);
+
+    // Checking if the two numbers are equal using the == operator
+    if (num1 == num2) {
+        printf("True\n");
+    } else {
+        printf("False\n");
+    }
+
+    return 0;
+}
