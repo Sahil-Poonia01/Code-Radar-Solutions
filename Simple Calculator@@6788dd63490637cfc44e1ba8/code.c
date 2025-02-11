d4fe586fe5c484f1e33d31fd9a94a5eb4e8e0512
Sahil-Poonia01 <sp6394159@gmail.com>
2025-02-11 12:50:54 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,61 +1,35 @@
 #include <stdio.h>
 
-// Function to perform the operation based on the operator
-void simple_calculator(float num1, float num2, char operator) {
-    float result;
-
-    // Check for the operator and perform the corresponding operation
-    switch (operator) {
-        case '+':
-            result = num1 + num2;
-            printf("Result: %.2f\n", result);
-            break;
-        case '-':
-            result = num1 - num2;
-            printf("Result: %.2f\n", result);
-            break;
-        case '*':
-            result = num1 * num2;
-            printf("Result: %.2f\n", result);
-            break;
-        case '/':
-            if (num2 != 0) {
-                result = num1 / num2;
-                printf("Result: %.2f\n", result);
-            } else {
-            
-            }
-            break;
-        default:
-            // Error message for invalid operator
-         
-    }
-}
-
 int main() {
     float num1, num2;
     char operator;
 
-    // Input from the user
+    // Input first number, operator, and second number
+    printf("Enter first number: ");
+    scanf("%f", &num1);
+    
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &operator);  // Space before %c to handle any leading whitespace
+    
+    printf("Enter second number: ");
+    scanf("%f", &num2);
 
-    if (scanf("%f", &num1) != 1) {
-      
-        return 1;
+    // Perform the operation based on the operator
+    if (operator == '+') {
+        printf("Result: %.2f\n", num1 + num2);
+    } else if (operator == '-') {
+        printf("Result: %.2f\n", num1 - num2);
+    } else if (operator == '*') {
+        printf("Result: %.2f\n", num1 * num2);
+    } else if (operator == '/') {
+        if (num2 != 0) {
+            printf("Result: %.2f\n", num1 / num2);
+        } else {
+            printf("Error: Division by zero is not allowed.\n");
+        }
+    } else {
+        printf("Error: Invalid operator.\n");
     }
-
-  
-    if (scanf(" %c", &operator) != 1) {
-     
-        return 1;
-    }
-
-    if (scanf("%f", &num2) != 1) {
-   
-        return 1;
-    }
-
-    // Perform the calculation based on the operator
-    simple_calculator(num1, num2, operator);
 
     return 0;
 }
